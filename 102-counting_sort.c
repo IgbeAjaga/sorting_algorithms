91905bd115284ad4497d86f2e1a277cf677f9320
IgbeAjaga <ajagaigbe144@gmail.com>
2023-06-27 07:52:25 -0700

    betty update

--- 102-counting_sort.c
+++ 102-counting_sort.c
@@ -10,55 +10,49 @@
  */
 void counting_sort(int *array, size_t size)
 {
-    int max_value = 0, i, *count_arr = NULL;
-    int *output = NULL;
+	int max_value = 0, i, *count_arr = NULL;
+	int *output = NULL;
 
-    if (array == NULL || size < 2)
-        return;
+	if (array == NULL || size < 2)
+		return;
 
-    /* Find the maximum value in the array */
-    for (i = 0; i < (int)size; i++)
-    {
-        if (array[i] > max_value)
-            max_value = array[i];
-    }
+	for (i = 0; i < (int)size; i++)
+	{
+		if (array[i] > max_value)
+			max_value = array[i];
+	}
 
-    count_arr = malloc(sizeof(int) * (max_value + 1));
-    if (count_arr == NULL)
-        return;
+	count_arr = malloc(sizeof(int) * (max_value + 1));
+	if (count_arr == NULL)
+		return;
 
-    /* Initialize the count array with 0 */
-    for (i = 0; i <= max_value; i++)
-        count_arr[i] = 0;
+	for (i = 0; i <= max_value; i++)
+		count_arr[i] = 0;
 
-    /* Count the occurrences of each element */
-    for (i = 0; i < (int)size; i++)
-        count_arr[array[i]]++;
+	for (i = 0; i < (int)size; i++)
+		count_arr[array[i]]++;
 
-    /* Modify the count array to have the actual position of each element */
-    for (i = 1; i <= max_value; i++)
-        count_arr[i] += count_arr[i - 1];
+	for (i = 1; i <= max_value; i++)
+		count_arr[i] += count_arr[i - 1];
 
-    print_array(count_arr, max_value + 1);
+	print_array(count_arr, max_value + 1);
 
-    output = malloc(sizeof(int) * size);
-    if (output == NULL)
-    {
-        free(count_arr);
-        return;
-    }
+	output = malloc(sizeof(int) * size);
+	if (output == NULL)
+	{
+		free(count_arr);
+		return;
+	}
 
-    /* Build the output array */
-    for (i = size - 1; i >= 0; i--)
-    {
-        output[count_arr[array[i]] - 1] = array[i];
-        count_arr[array[i]]--;
-    }
+	for (i = size - 1; i >= 0; i--)
+	{
+		output[count_arr[array[i]] - 1] = array[i];
+		count_arr[array[i]]--;
+	}
 
-    /* Copy the output array back to the original array */
-    for (i = 0; i < (int)size; i++)
-        array[i] = output[i];
+	for (i = 0; i < (int)size; i++)
+		array[i] = output[i];
 
-    free(output);
-    free(count_arr);
+	free(output);
+	free(count_arr);
 }
